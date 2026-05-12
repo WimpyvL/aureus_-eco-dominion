165cc0171a9e1fcc92cc0035efa0f4feb67079b1
Klaasvaakie <49187633+WimpyvL@users.noreply.github.com>
2026-05-12 02:18:28 +0200

    Load Deep Ledger render marker overlay

--- index.tsx
+++ index.tsx
@@ -13,6 +13,7 @@ import './game/surveyDrillAccessPatch';
 import './game/deepLedgerTileInspectorPatch';
 import './game/deepLedgerDigActionPatch';
 import './game/deepLedgerCollapseRiskPatch';
+import './game/deepLedgerRenderMarkersPatch';
 import ReactDOM from 'react-dom/client';
 import { Analytics } from "@vercel/analytics/react";
 import { BrowserRouter } from 'react-router-dom';
